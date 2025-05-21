1bccb0373852f3ac359467bee6b593db17a53cfc
Danila <Danila@DESKTOP-44LP0PV>
2025-05-21 20:58:10 +0300

    classwork_commit

---
+++ peregruzka/main.cpp
@@ -0,0 +1,154 @@
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+void FillRand(int arr[], const int n, int MinRand = 0, int MaxRand = 100);
+void FillRand(double arr[], const int n, int MinRand = 0, int MaxRand = 100);
+void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+
+int Sum(int arr[], const int n);
+double AVG(int arr[], const int n);
+
+int MinValueIn(int arr[], const int n);
+int MaxValueIn(int arr[], const int n);
+
+void ShiftLeft(int arr[], const int n, const int number_of_shifts);
+void ShiftRight(int arr[], const int n, const int number_of_shifts);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	const int n = 10;
+	int arr[n] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,};
+	/*FillRand(arr, n);*/
+	Print(arr, n);
+	Sort(arr, n);
+	Print(arr, n);
+	cout << "\n" << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "\n" << "Среднее арифметическое массива: " << AVG(arr, n) << endl;
+	cout << "\n" << "Вывод Минимального значения массива: " << MinValueIn(arr, n) << endl;
+	cout << "\n" << "Вывод Максимального значения массива: " << MaxValueIn(arr, n) << endl;
+	int number_of_shifts;
+	cout << "\n" << "Сдвиг массива влево - "; cin >> number_of_shifts; 
+	ShiftLeft(arr, n, number_of_shifts);
+	Print(arr, n);
+	cout << "\n" << "Сдвиг массива вправо - "; cin >> number_of_shifts;
+	ShiftRight(arr, n, number_of_shifts);
+	Print(arr, n);
+}
+
+void FillRand(int arr[], const int n, int MinRand, int MaxRand)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (MaxRand - MinRand) + MinRand;
+	}
+}
+void FillRand(double arr[], const int n, int MinRand, int MaxRand)
+{
+	MinRand *= 100;
+	MaxRand *= 100;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (MaxRand - MinRand) + MinRand;
+		arr[i] /= 100;
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void Print(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void Sort(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+void Sort(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				double buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+int Sum(int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+double AVG(int arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
+int MinValueIn(int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[0];
+	}
+	return min;
+}
+int MaxValueIn(int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+} // ctrl+r
+void ShiftLeft(int arr[], const int n, const int number_of_shifts)
+{
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 1; i < n; i++)
+		{
+			arr[i - 1] = arr[i];
+		}
+		arr[n - 1] = buffer;
+	}
+}
+void ShiftRight(int arr[], const int n, const int number_of_shifts)
+{
+	ShiftLeft(arr, n, n - number_of_shifts);
+}
